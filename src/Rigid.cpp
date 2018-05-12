13633b7dbd772b0a7400cd18ae8b7e794cb04132
Ying Wang <ying.wang@tamu.edu>
2018-05-12 01:32:57 -0500

    try rkf45

--- src/Rigid.cpp
+++ src/Rigid.cpp
@@ -14,14 +14,13 @@
 #include "MatrixStack.h"
 
 using namespace std;
+using namespace Eigen;
 
 Rigid::Rigid() :
 	r(1.0),
 	m(1.0),
 	i(-1),
-	x(0.0, 0.0, 0.0),
-	v(0.0, 0.0, 0.0),
-	fixed(true)
+	fixed(false)
 {
 
 }
@@ -30,13 +29,10 @@ Rigid::Rigid(const shared_ptr<Shape> s) :
 	r(1.0),
 	m(1.0),
 	i(-1),
-	x(0.0, 0.0, 0.0),
-	v(0.0, 0.0, 0.0),
-	fixed(true),
-	normal(0.0, 0.0, 0.0),
+	fixed(false),
 	box(s)
 {
-
+	twist.setZero();
 }
 
 Rigid::~Rigid()
@@ -45,14 +41,12 @@ Rigid::~Rigid()
 
 void Rigid::tare()
 {
-	x0 = x;
-	v0 = v;
+	twist.setZero();
 }
 
 void Rigid::reset()
 {
-	x = x0;
-	v = v0;
+	twist.setZero();
 }
 
 void Rigid::step(double h) {
@@ -63,10 +57,114 @@ void Rigid::draw(shared_ptr<MatrixStack> MV, const shared_ptr<Program> prog) con
 {
 	if (box) {
 		MV->pushMatrix();
-		MV->translate(x(0), x(1), x(2));
+		//MV->translate(x(0), x(1), x(2));
 		MV->scale(r);
 		glUniformMatrix4fv(prog->getUniform("MV"), 1, GL_FALSE, glm::value_ptr(MV->topMatrix()));
 		box->draw(prog);
 		MV->popMatrix();
 	}
+}
+
+Matrix4d Rigid::inverse(const Matrix4d &E)
+{
+	Matrix4d Einv = Matrix4d::Identity();
+	Matrix3d R = E.block<3, 3>(0, 0);
+	Vector3d p = E.block<3, 1>(0, 3);
+	Matrix3d Rt = R.transpose();
+	Einv.block<3, 3>(0, 0) = Rt;
+	Einv.block<3, 1>(0, 3) = -Rt * p;
+	return Einv;
+}
+
+Matrix3x6d Rigid::gamma(const Eigen::Vector3d &r)
+{
+	Matrix3x6d G = Matrix3x6d::Zero();
+	G.block<3, 3>(0, 0) = bracket3(r).transpose();
+	G.block<3, 3>(0, 3) = Matrix3d::Identity();
+	return G;
+}
+
+Matrix6d Rigid::adjoint(const Matrix4d &E)
+{
+	Matrix6d Ad = Matrix6d::Zero();
+	Matrix3d R = E.block<3, 3>(0, 0);
+	Vector3d p = E.block<3, 1>(0, 3);
+	Ad.block(0, 0, 3, 3) = R;
+	Ad.block(3, 0, 3, 3) = bracket3(p) * R;
+	Ad.block(3, 3, 3, 3) = R;
+	return Ad;
+}
+
+Matrix3d Rigid::bracket3(const Vector3d &a)
+{
+	Matrix3d A = Matrix3d::Zero();
+	A(0, 1) = -a(2);
+	A(0, 2) = a(1);
+	A(1, 0) = a(2);
+	A(1, 2) = -a(0);
+	A(2, 0) = -a(1);
+	A(2, 1) = a(0);
+	return A;
+}
+
+Matrix4d Rigid::bracket6(const Vector6d &a)
+{
+	Matrix4d A = Matrix4d::Zero();
+	A.block<3, 3>(0, 0) = bracket3(a.segment<3>(0));
+	A.block<3, 1>(0, 3) = a.segment<3>(3);
+	return A;
+}
+
+Vector3d Rigid::unbracket3(const Matrix3d &A)
+{
+	Vector3d a;
+	a(0) = A(2, 1);
+	a(1) = A(0, 2);
+	a(2) = A(1, 0);
+	return a;
+}
+
+Vector6d Rigid::unbracket6(const Matrix4d &A)
+{
+	Vector6d a;
+	a.segment<3>(0) = unbracket3(A.block<3, 3>(0, 0));
+	a(3) = A(0, 3);
+	a(4) = A(1, 3);
+	a(5) = A(2, 3);
+	return a;
+}
+
+Matrix4d Rigid::integrate(const Matrix4d &E0, const VectorXd &phi, double h)
+{
+	Matrix3d I = Matrix3d::Identity();
+	Vector3d w = phi.segment<3>(0);
+	Vector3d v = phi.segment<3>(3);
+	Matrix4d phib = Matrix4d::Identity();
+	phib.block<3, 1>(0, 3) = h*v;
+	double wlen = w.norm();
+	if (wlen > 1e-10) {
+		w /= wlen;
+		v /= wlen;
+		// Rodrigues formula %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		double wX = w(0);
+		double wY = w(1);
+		double wZ = w(2);
+		double c = cos(wlen * h);
+		double s = sin(wlen * h);
+		double c1 = 1.0 - c;
+		Matrix3d R;
+		R << c + wX * wX * c1, -wZ * s + wX * wY * c1, wY * s + wX * wZ * c1,
+			wZ * s + wX * wY * c1, c + wY * wY * c1, -wX * s + wY * wZ * c1,
+			-wY * s + wX * wZ * c1, wX * s + wY * wZ * c1, c + wZ * wZ * c1;
+		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		Matrix3d A = I - R;
+		Vector3d cc = w.cross(v);
+		Vector3d d = A * cc;
+		double wv = w.dot(v);
+		Vector3d p = (wv * wlen * h) * w + d;
+		phib.block<3, 3>(0, 0) = R;
+		phib.block<3, 1>(0, 3) = p;
+		//cout << phib << endl;
+	}
+	return E0 * phib;
 }
--- src/Rigid.h
+++ src/Rigid.h
@@ -30,15 +30,21 @@ public:
 	void step(double h);
 	void draw(std::shared_ptr<MatrixStack> MV, const std::shared_ptr<Program> p) const;
 
+	static Eigen::Matrix4d inverse(const Eigen::Matrix4d &E);
+	static Matrix3x6d gamma(const Eigen::Vector3d &r);
+	static Matrix6d adjoint(const Eigen::Matrix4d &E);
+	static Eigen::Matrix3d bracket3(const Eigen::Vector3d &a);
+	static Eigen::Matrix4d bracket6(const Vector6d &a);
+	static Eigen::Vector3d unbracket3(const Eigen::Matrix3d &A);
+	static Vector6d unbracket6(const Eigen::Matrix4d &A);
+	static Eigen::Matrix4d integrate(const Eigen::Matrix4d &E0, const Eigen::VectorXd &phi, double h);
+
 	double r; // radius
 	double m; // mass
-	int i;  // starting index
-	Eigen::Vector3d x0; // initial position
-	Eigen::Vector3d v0; // initial velocity
-	Eigen::Vector3d x;  // position
-	Eigen::Vector3d v;  // velocity
-	bool fixed;
-	Eigen::Vector3d normal;
+	int i;  // index
+	Vector6d twist;
+
+	bool fixed;	
 
 private:
 	const std::shared_ptr<Shape> box;
